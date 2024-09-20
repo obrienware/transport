61c781884f707aef0873f4e08bdb760d57979a02
Richard OBrien <richard@obrienware.com>
2024-09-19 18:16:47 -0600

    Fixed Users to pass tests

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
@@ -43,10 +44,5 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function method_name()
-    {
-        
     }
 }
