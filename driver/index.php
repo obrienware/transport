5d40a20150d7d720733253ee9b30658bd09ff6b6
Richard OBrien <richard@obrienware.com>
2024-11-25 12:28:09 -0700

    Made some iOS additions for /driver

--- driver/index.php
+++ driver/index.php
@@ -3,6 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/images/logo-152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/images/logo-180.png">
+  <link rel="apple-touch-icon" sizes="167x167" href="/images/logo-167.png">
+  <meta name="apple-mobile-web-app-title" content="Transport">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+
   <title>Transport</title>
   <!-- We want the full-blown jQuery lib so we can take advantage of ajax loading, etc. -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
