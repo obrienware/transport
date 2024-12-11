0c732f3e774f6fba9f0219ca1bc8afc70d82f2f9
Richard OBrien <richard@obrienware.com>
2024-12-11 08:45:50 -0700

    Improved the look (and layout) of the dashboard items

--- inc.dash-finalize.php
+++ inc.dash-finalize.php
@@ -19,20 +19,24 @@ WHERE
 ";
 ?>
 <?php if ($rs = $db->get_results($sql)): ?>
-  <div class="card mb-3" style="width:fit-content">
-    <h5 class="card-header">Trips Not Yet Finalized</h5>
-    <div class="card-body bg-danger-subtle text-center">
-      <sup>*</sup>Only once trips are finalized do all relavent<br/>parties start receiving notifications
+  <div class="row">
+    <div class="col-6">
+      <div class="card mb-3">
+        <h5 class="card-header">Upcoming Trips Not Yet Finalized</h5>
+        <div class="card-body bg-danger-subtle text-center">
+          <sup>*</sup>Only once trips are finalized do all relavent parties start receiving notifications
+        </div>
+        <ul class="list-group list-group-flush">
+          <?php foreach ($rs as $item): ?>
+            <li class="list-group-item d-flex justify-content-between">
+              <div>
+                <button class="btn p-0" onclick="app.openTab('edit-trip', 'Trip (edit)', 'section.edit-trip.php?id=<?=$item->id?>');"><?=$item->summary?></button>
+              </div>
+              <div class="ms-2 badge bg-primary datetime align-self-center"><?=Date('D', strtotime($item->start_date))?></div>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
     </div>
-    <ul class="list-group list-group-flush">
-      <?php foreach ($rs as $item): ?>
-        <li class="list-group-item d-flex justify-content-between">
-          <div>
-            <button class="btn p-0" onclick="app.openTab('edit-trip', 'Trip (edit)', 'section.edit-trip.php?id=<?=$item->id?>');"><?=$item->summary?></button>
-          </div>
-          <div class="ms-2 badge bg-primary datetime align-self-center"><?=Date('D', strtotime($item->start_date))?></div>
-        </li>
-      <?php endforeach; ?>
-    </ul>
   </div>
 <?php endif;?>
--- inc.dash-vehicles.php
+++ inc.dash-vehicles.php
@@ -19,74 +19,75 @@ $sql = "
 ";
 ?>
 <?php if ($rs = $db->get_results($sql)): ?>
-  <div class="card text-bg-warning mb-4">
-    <h5 class="card-header overflow-hidden">
-      Vehicles needing attention!
-    </h5>
-    <table class="table table-sm mb-0 table-bordered">
-      <thead>
-        <tr class="align-middle table-primary">
-          <th rowspan="2" class="text-center">As of</th>
-          <th rowspan="2" class="text-center">Vehicle</th>
-          <th rowspan="2" class="text-center">Location</th>
-          <th colspan="4" class="text-center">Needs</th>
-        </tr>
-        <tr class="table-primary">
-          <th class="text-center">Fuel</th>
-          <th class="text-center">Clean (ext)</th>
-          <th class="text-center">Clean (int)</th>
-          <th class="text-center">Restock</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach ($rs as $item): ?>
-          <tr>
-            <td class="datetime short"><?=$item->last_update?></td>
-            <td>
-              <button class="btn p-0" onclick="app.openTab('view-vehicle', 'Vehicle', 'section.view-vehicle.php?id=<?=$item->id?>');">
-                <?php if ($item->check_engine): ?>
-                  <i class="fa-duotone fa-regular fa-engine-warning text-danger fa-xl"></i>
-                <?php endif;?>
-                <?=$item->name?>
-              </button>
-            </td>
-            <td><?=$item->location ?: 'unverified'?></td>
-            <td class="text-center">
-              <?php if ($item->fuel_level == null): ?>
-                -
-              <?php else: ?>
-                <?php if ($item->fuel_level <= 25): ?>
-                  <span class="badge bg-danger fs-6"><?=$item->fuel_level?>%</span>
-                <?php else:?>
-                  <?=$item->fuel_level?>%
-                <?php endif; ?>
-              <?php endif; ?>
-            </td>
-            <td class="text-center">
-              <?php if ($item->clean_exterior === 0): ?>
-                <div class="badge bg-danger fs-6">YES</div>
-              <?php else: ?>
-                -
-              <?php endif; ?>
-            </td>
-            <td class="text-center">
-              <?php if ($item->clean_interior === 0): ?>
-                <div class="badge bg-danger fs-6">YES</div>
-              <?php else: ?>
-                -
-              <?php endif; ?>
-            </td>
-            <td class="text-center">
-              <?php if ($item->restock === 1): ?>
-                <div class="badge bg-danger fs-6">YES</div>
-              <?php else: ?>
-                -
-              <?php endif; ?>
-            </td>
-          </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
+
+  <div class="row row-cols-2 rows-cols-md-3 row-cols-xl-4 row-cols-xxl-5 g-4 mb-4">
+    <?php foreach ($rs as $item): ?>
+
+      <div class="col">
+        <div class="card">
+          <div class="card-header" style="background-color:<?=$item->color?>">
+            <button style="color: #<?=readableColor($item->color)?> !important" class="btn p-0" onclick="app.openTab('view-vehicle', 'Vehicle', 'section.view-vehicle.php?id=<?=$item->id?>');">
+            <?=$item->name?>
+            </button>
+          </div>
+          <ul class="list-group list-group-flush">
+
+            <?php if ($item->check_engine): ?>
+              <li class="list-group-item">
+                <i class="fa-xl fa-solid fa-engine-warning fa-fw" style="color: orangered"></i> Check Engine
+              </li>
+            <?php endif; ?>
+
+            <?php if (!is_null($item->fuel_level) && $item->fuel_level <= 25): ?>
+              <li class="list-group-item">
+                <i class="fa-xl fa-solid fa-triangle-exclamation fa-fw" style="color: orangered"></i> Fuel Level: <?=$item->fuel_level?>%
+              </li>
+            <?php endif;?>
+
+            <?php if ($item->clean_interior === 0): ?>
+              <li class="list-group-item">
+                <i class="fa-xl fa-solid fa-triangle-exclamation fa-fw" style="color: orangered"></i> Needs interior cleaning
+              </li>
+            <?php endif; ?>
+
+            <?php if ($item->clean_exterior === 0): ?>
+              <li class="list-group-item">
+                <i class="fa-xl fa-solid fa-triangle-exclamation fa-fw" style="color: orangered"></i> Needs exterior cleaning
+              </li>
+            <?php endif; ?>
+
+            <?php if ($item->restock === 1): ?>
+              <li class="list-group-item">
+                <i class="fa-xl fa-solid fa-triangle-exclamation fa-fw" style="color: orangered"></i> Needs restocking
+              </li>
+            <?php endif; ?>
+
+          </ul>
+        </div>
+      </div>
+
+    <?php endforeach; ?>
   </div>
 
 <?php endif;?>
+
+<?php
+function readableColor($bg)
+{
+  $bg = str_replace('#', '', $bg);
+  $r = hexdec(substr($bg, 0, 2));
+  $g = hexdec(substr($bg, 2, 2));
+  $b = hexdec(substr($bg, 4, 2));
+
+  $squared_contrast = (
+    $r * $r * .299 +
+    $g * $g * .587 +
+    $b * $b * .114
+  );
+
+  if ($squared_contrast > pow(170, 2)) {
+    return '000000';
+  } else {
+    return 'FFFFFF';
+  }
+}
