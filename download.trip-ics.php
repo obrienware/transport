291dab9ceda0efb378ff4a1b4317f94abd6ae98b
Richard OBrien <richard@obrienware.com>
2024-12-12 12:48:57 -0700

    Option to download an iCalendar (.ics) file for a trip.

---
+++ download.trip-ics.php
@@ -0,0 +1,35 @@
+<?php
+header('Content-Type: text/calendar; charset=utf-8');
+header('Content-Disposition: attachment; filename=invite.ics');
+
+require_once 'class.trip.php';
+require_once 'class.ics.php';
+$trip = new Trip($_REQUEST['id']);
+
+$description = "";
+$description .= "Using ".$trip->vehicle->name.": ".$trip->vehiclePUOptions.' - '.$trip->vehicleDOOptions."\\n";
+$description .= "PU ".$trip->guests." at ".$trip->puLocation->name."\\n";
+$description .= "DO ".$trip->doLocation->name."\\n";
+if ($trip->flightNumber) {
+  $description .= "Flight ".$trip->airline->name." ".$trip->airline->flightNumberPrefix.$trip->flightNumber." ";
+  if ($trip->ETA) {
+    $description .= "ETA ".Date('i:ga', strtotime($trip->ETA))."\\n";
+  } else {
+    $description .= "ETD ".Date('i:ga', strtotime($trip->ETD))."\\n";
+  }
+}
+$description .= "Contact: ".$trip->guest->getName()." ".$trip->guest->phoneNumber."\\n";
+if ($trip->driverNotes) {
+  $description .= "Additional Driver Notes:\\n";
+  $description .= $trip->driverNotes."\\n";
+}
+
+$ics = new ICS([
+  'dtstart' => $trip->startDate,
+  'dtend' => $trip->endDate,
+  'description' => $description,
+  'summary' => $trip->summary,
+  'location' => str_replace("\n", "\\n", $trip->puLocation->mapAddress)
+]);
+
+echo $ics->to_string();
