ea6ffe58982d86309d47de869c458c9130cdd17a
Richard OBrien <richard@obrienware.com>
2024-12-13 10:31:39 -0700

    Include an ics in the driver email when finalizing a trip.

--- api/post.finalize-trip.php
+++ api/post.finalize-trip.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-Type: application/json');
+require_once 'class.ics.php';
 require_once 'class.config.php';
 require_once 'class.email.php';
 require_once 'class.utils.php';
@@ -27,6 +28,36 @@ if ($trip->finalized == 0) {
   include '../inc.trip-guest-sheet.php';
   $filename2 = $_SERVER['DOCUMENT_ROOT'].'/tripsheets/'.$trip->tripId.'-trip-guest-sheet.pdf';
   $pdf->output('F', $filename2);
+
+  // Generate ics file
+  $description = "";
+  $description .= "Using ".$trip->vehicle->name.": ".$trip->vehiclePUOptions.' - '.$trip->vehicleDOOptions."\\n\\n";
+  $description .= "PU ".$trip->guests." at ".$trip->puLocation->name."\\n\\n";
+  $description .= "DO ".$trip->doLocation->name."\\n\\n";
+  if ($trip->flightNumber) {
+    $description .= "Flight ".$trip->airline->name." ".$trip->airline->flightNumberPrefix.$trip->flightNumber." ";
+    if ($trip->ETA) {
+      $description .= "ETA ".Date('g:ia', strtotime($trip->ETA))."\\n\\n";
+    } else {
+      $description .= "ETD ".Date('g:ia', strtotime($trip->ETD))."\\n\\n";
+    }
+  }
+  $description .= "Contact: ".$trip->guest->getName()." ".$trip->guest->phoneNumber."\\n\\n";
+  if ($trip->driverNotes) {
+    $description .= "Additional Driver Notes:\\n";
+    $description .= str_replace("\n", "\\n", $trip->driverNotes)."\\n";
+  }
+  
+  $ics = new ICS([
+    'dtstart' => $trip->startDate,
+    'dtend' => $trip->endDate,
+    'description' => $description,
+    'summary' => $trip->summary,
+    'location' => str_replace("\n", "\\n", $trip->puLocation->mapAddress),
+    'url' => 'https://'.$_SERVER['HTTP_HOST'].'/print.trip-driver-sheet.php?id='.$trip->tripId
+  ]);
+  
+
 
   // Email to the requestor (include the guest sheet to pass along)
   $requestorName = $trip->requestor->firstName;
@@ -60,6 +91,8 @@ Transportation Team
   $tripDate = Date('m/d/Y', strtotime($trip->pickupDate));
   $email = new Email();
   $email->addAttachment($filename1);
+  $ical = $ics->to_string();
+  $email->AddStringAttachment("$ical", "calendar-item.ics", "base64", "text/calendar; charset=utf-8; method=REQUEST");
   $email->setSubject('A trip has been assigned to you: '.$trip->summary);
   $email->setContent("
 Hello {$driverName},
