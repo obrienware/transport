8271ba4db0442b41897b6f98c9fbb1a50ca35d27
Richard OBrien <richard@obrienware.com>
2024-12-14 20:47:05 -0700

    Redesigned the flights page

--- section.list-flights.php
+++ section.list-flights.php
@@ -6,13 +6,14 @@ if (!isset($db)) $db = new data();
 
 $sql = "
 SELECT 
-  t.summary,
+  t.summary, t.guests,
   t.pickup_date,
   CASE WHEN t.ETA IS NOT NULL THEN t.ETA ELSE t.ETD END AS target_datetime,
   CASE WHEN t.ETA IS NOT NULL THEN 'arrival' ELSE 'departure' END AS `type`,
   CASE WHEN t.ETA IS NOT NULL THEN a.iata ELSE b.iata END AS iata,
   CONCAT(l.flight_number_prefix, t.flight_number) AS flight_number,
-  l.name AS airline
+  l.name AS airline,
+  l.image_filename
 FROM trips t
 LEFT OUTER JOIN airlines l ON l.id = t.airline_id
 LEFT OUTER JOIN locations a ON a.id = t.pu_location
@@ -20,87 +21,113 @@ LEFT OUTER JOIN locations b ON b.id = t.do_location
 WHERE
  	(t.eta IS NOT NULL OR t.etd IS NOT NULL)
 	AND
-	(t.eta IS NULL OR DATE(eta) >= DATE_SUB(CURDATE(), INTERVAL 3 DAY))
+	(t.eta IS NULL OR DATE(eta) >= CURDATE())
 	AND
-	(t.etd IS NULL OR DATE(etd) >= DATE_SUB(CURDATE(), INTERVAL 3 DAY))	
+	(t.etd IS NULL OR DATE(etd) >= CURDATE())	
 	AND t.archived IS NULL
-  AND DATE(t.pickup_date) < DATE_ADD(CURDATE(), INTERVAL 7 DAY)
+  AND DATE(t.pickup_date) < DATE_ADD(CURDATE(), INTERVAL 7 DAY) -- Looking 7 days ahead
 ORDER BY COALESCE(t.eta, t.etd) -- This is brilliant! Orders by either ETA OR ETD where the other is NULL!
 ";
 
+function showFlightsFor($iata, $type)
+{
+  global $db;
+  global $sql;
+  $count = 0;
+  if ($rs = $db->get_results($sql)) {
+    foreach ($rs as $item) {
+      if ($item->type !== $type) continue;
+      if ($item->iata !== $iata) continue;
+      $flight = Flight::getFlightStatus($item->flight_number, $item->type, $item->iata, Date('Y-m-d', strtotime($item->target_datetime)));
+      if (!$flight->flight_number) continue; // exclude flights we didn't track.
+      $count++;
+      echo '<tr class="border-0 border-5 border-top">';
+
+      echo '<td class="fit text-center align-middle border-bottom" rowspan="2">';
+      echo '<div class="badge bg-danger">'.Date('M', strtotime($item->pickup_date)).'</div>';
+      echo '<div>'.Date('D', strtotime($item->pickup_date)).'</div>';
+      echo '<div class="fs-4 fw-bold font-monospace">'.Date('d', strtotime($item->pickup_date)).'</div>';
+      echo '</td>';
+
+      echo '<td class="border-bottom">';
+      echo '<img src="/images/airlines/'.$item->image_filename.'" class="img-fluid" style="max-height:30px">';
+      echo '</td>';
+
+      echo '<td class="border-bottom align-middle">';
+      echo $flight->flight_number;
+      echo '</td>';
+
+      echo '<td class="border-bottom align-middle">';
+      echo $flight->airport_origin_iata.' <i class="fa-solid fa-circle-arrow-right"></i> '.$flight->airport_destination_iata;
+      echo '</td>';
+
+      $colorClass = '';
+      if ($flight->status_icon === 'green') $colorClass = 'border-success';
+      if ($flight->status_icon === 'yellow') $colorClass = 'border-warning';
+      if ($flight->status_icon === 'red') $colorClass = 'border-danger';
+      echo '<td rowspan="2" class="text-center align-middle border-0 border-5 border-start '.$colorClass.'">';
+      if ($type === 'arrival') {
+        if ($flight->real_arrival) echo '<div class="fw-bold">'.Date('g:ia', strtotime($flight->real_arrival)).'</div><div>Actual</div>';
+        elseif ($flight->estimated_arrival) echo '<div class="fw-bold">'.Date('g:ia', strtotime($flight->estimated_arrival)).'</div><div>Estimated</div>';
+        elseif ($flight->scheduled_arrival) echo '<div class="fw-bold">'.Date('g:ia', strtotime($flight->scheduled_arrival)).'</div><div>Scheduled</div>';
+      }
+      if ($type === 'departure') {
+        if ($flight->real_departure) echo '<div class="fw-bold">'.Date('g:ia', strtotime($flight->real_departure)).'</div><div>Actual</div>';
+        elseif ($flight->estimated_departure) echo '<div class="fw-bold">'.Date('g:ia', strtotime($flight->estimated_departure)).'</div><div>Estimated</div>';
+        elseif ($flight->scheduled_departure) echo '<div class="fw-bold">'.Date('g:ia', strtotime($flight->scheduled_departure)).'</div><div>Scheduled</div>';        
+      }
+      echo '</td>';
+
+      echo '</tr>';
+
+      echo '<tr>';
+      echo '<td colspan="3">';
+      echo $item->guests;
+      echo '</td>';
+      echo '</tr>';
+    }
+  }
+  if ($count === 0) {
+    echo '<tr>';
+    echo '<td class="text-center">';
+    echo 'No up-coming flights being tracked at this time';
+    echo '</td>';
+    echo '</tr>';
+  }
+}
 
 ?>
 <div class="container-fluid">
   <h1>Flight Statuses</h1>
-  <?php if ($rs = $db->get_results($sql)): ?>
 
-    <?php foreach ($rs as $item): ?>
-      <?php 
-        $flight = Flight::getFlightStatus($item->flight_number, $item->type, $item->iata, Date('Y-m-d', strtotime($item->target_datetime)));
-        if (!$flight->flight_number) continue; // exclude flights we didn't track.
-        switch ($flight->status_icon) {
-          case 'green':
-            $tableClass = "table-success";
-            break;
-          case 'yellow':
-            $tableClass = "table-warning";
-            break;
-          case 'red':
-            $tableClass = "table-danger";
-            break;
-          default:
-            $tableClass = "table-secondary";
-            break;
-        }
-        $classArrival = '';
-        $classDeparture = '';
-        if ($item->type == 'arrival') $classArrival = 'bg-info-subtle';
-        if ($item->type == 'departure') $classDeparture = 'bg-info-subtle';
-      ?>
-      <div class="mb-3">
-        <table class="table table-sm table-bordered <?=$tableClass?>">
-          <caption class="caption-top"><?=Date('m/d/Y', strtotime($item->pickup_date)).': '.$item->summary?></caption>
-          <thead>
-            <tr>
-              <th data-bs-toggle="tooltip" data-bs-title="Flight Number">Flight</th>
-              <th data-bs-toggle="tooltip" data-bs-title="Active">Act</th>
-              <th data-bs-toggle="tooltip" data-bs-title="Status">Status</th>
-              <th class="<?=$classDeparture?>" data-bs-toggle="tooltip" data-bs-title="Origin"><i class="fa-duotone fa-solid fa-plane-departure"></i> DEP</th>
-              <th class="<?=$classDeparture?>" data-bs-toggle="tooltip" data-bs-title="Scheduled Departure">Sch</th>
-              <th class="<?=$classDeparture?>" data-bs-toggle="tooltip" data-bs-title="Estimated Departure">Est</th>
-              <th class="<?=$classDeparture?>" data-bs-toggle="tooltip" data-bs-title="Actual Departure">Act</th>
-              <th class="fit">&nbsp;</th>
-              <th class="<?=$classArrival?>" data-bs-toggle="tooltip" data-bs-title="Destination"><i class="fa-duotone fa-solid fa-plane-arrival"></i> ARR</th>
-              <th class="<?=$classArrival?>" data-bs-toggle="tooltip" data-bs-title="Scheduled Arrival">Sch</th>
-              <th class="<?=$classArrival?>" data-bs-toggle="tooltip" data-bs-title="Estimated Arrival">Est</th>
-              <th class="<?=$classArrival?>" data-bs-toggle="tooltip" data-bs-title="Actual Arrival">Act</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr class="<?=$tableClass?>">
-              <td data-bs-toggle="tooltip" data-bs-title="<?=$item->airline?>"><?=$flight->flight_number?></td>
-              <td><?=$flight->status_live ? '<i class="fa-solid fa-check"></i>' : ''?></td>
-              <td><?=$flight->status_text?></td>
-              <td data-bs-toggle="tooltip" data-bs-title="<?=$flight->airport_origin?>"><?=$flight->airport_origin_iata?></td>
-              <td><?=$flight->scheduled_departure ? Date('g:ia', strtotime($flight->scheduled_departure)) : '-'?></td>
-              <td><?=$flight->estimated_departure ? Date('g:ia', strtotime($flight->estimated_departure)) : '-'?></td>
-              <td><?=$flight->real_departure ? Date('g:ia', strtotime($flight->real_departure)) : '-'?></td>
-              <td class="fit">&nbsp;</td>
-              <td data-bs-toggle="tooltip" data-bs-title="<?=$flight->airport_destination?>"><?=$flight->airport_destination_iata?></td>
-              <td><?=$flight->scheduled_arrival ? Date('g:ia', strtotime($flight->scheduled_arrival)) : '-'?></td>
-              <td><?=$flight->estimated_arrival ? Date('g:ia', strtotime($flight->estimated_arrival)) : '-'?></td>
-              <td><?=$flight->real_arrival ? Date('g:ia', strtotime($flight->real_arrival)) : '-'?></td>
-            </tr>
-          </tbody>
-        </table>
+  <?php if ($airports = $db->get_results("SELECT * FROM airports")): ?>
+    <?php foreach ($airports as $airport): ?>
+      <div class="row mb-4">
+        <div class="col">
+          <div class="card text-bg-primary">
+            <div class="card-header d-flex justify-content-between">
+              <div><?=$airport->iata?>: <?=$airport->name?></div>
+              <div><i class="fa-duotone fa-solid fa-plane-arrival"></i> ARRIVALS</div>
+            </div>
+            <table class="table mb-0" id="<?=$airport->iata?>-arrival">
+              <?=showFlightsFor($airport->iata, 'arrival'); ?>
+            </table>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card text-bg-primary">
+            <div class="card-header d-flex justify-content-between">
+              <div><?=$airport->iata?>: <?=$airport->name?></div>
+              <div><i class="fa-duotone fa-solid fa-plane-departure"></i> DEPARTURES</div>
+            </div>
+            <table class="table mb-0" id="<?=$airport->iata?>-departure">
+              <?=showFlightsFor($airport->iata, 'departure'); ?>
+            </table>
+          </div>
+        </div>
       </div>
     <?php endforeach; ?>
+  <?php endif;?>
 
-  <?php else: ?>
-
-    <div class="alert alert-info my-5">
-      <p class="lead">There are no flights currently being tracked.</p>
-    </div>
-
-  <?php endif; ?>
 </div>
