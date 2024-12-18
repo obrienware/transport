b647bfce45766fae4bf7c4534a107cadbc9be036
Richard OBrien <richard@obrienware.com>
2024-12-18 13:52:50 -0700

    Added vehicle update to audit trail

--- driver/api/post.vehicle-update.php
+++ driver/api/post.vehicle-update.php
@@ -1,5 +1,7 @@
 <?php
 header('Content-Type: application/json');
+date_default_timezone_set($_ENV['TZ'] ?: 'America/Denver');
+require_once 'class.audit.php';
 require_once 'class.vehicle.php';
 $json = json_decode(file_get_contents("php://input"));
 
@@ -11,6 +13,14 @@ if (isset($json->restock)) $vehicle->restock = $json->restock;
 if (isset($json->cleanInterior)) $vehicle->cleanInterior = $json->cleanInterior;
 if (isset($json->cleanExterior)) $vehicle->cleanExterior = $json->cleanExterior;
 if (isset($json->fuelLevel)) $vehicle->fuelLevel = $json->fuelLevel;
+$vehicle->lastUpdate = Date('Y-m-d H:i:s');
+$vehicle->lastUpdatedBy = $_SESSION['user']->username;
 $vehicle->save();
+
+$before = $vehicle->getState();
+$vehicle->getVehicle($vehicle->getId());
+$after = $vehicle->getState();
+$description = 'Updated vehicle condition/status: '.$vehicle->name;
+Audit::log('update', 'vehicles', $description, $before, $after);
 
 echo json_encode(['result' => true]);
