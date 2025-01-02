df9eb71a49ea8912689beaa6df95a738168dafec
Richard OBrien <richard@obrienware.com>
2025-01-02 12:26:30 -0700

    Fixed: field name has changed from uploaded to created

--- classes/class.vehicle-document.php
+++ classes/class.vehicle-document.php
@@ -173,7 +173,7 @@ class VehicleDocument
 	static public function getDocuments($vehicleId): array
   {
     $db = new data();
-    $query = "SELECT * FROM vehicle_documents WHERE vehicle_id = :vehicle_id AND archived IS NULL ORDER BY uploaded";
+    $query = "SELECT * FROM vehicle_documents WHERE vehicle_id = :vehicle_id AND archived IS NULL ORDER BY created";
     $params = ['vehicle_id' => $vehicleId];
     return $db->get_rows($query, $params);
   }
