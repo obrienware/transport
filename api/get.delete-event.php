30f0f6ea2211885fcc9165ef0cc9b2020d356557
Richard OBrien <richard@obrienware.com>
2025-01-30 12:07:39 -0700

    Refactor email notification logic for event deletion and confirmation; add null checks for requestor and drivers

--- api/get.delete-event.php
+++ api/get.delete-event.php
@@ -34,28 +34,30 @@ function notifyParticipants(Event $event): void
   $endDate = Date('m/d/Y', strtotime($event->endDate));
 
   // Email to the requestor
-  $template = new Template(EmailTemplates::get('Email Requestor Event Deleted'));
-  $templateData = [
-    'name' => $event->requestor->firstName,
-    'eventName' => $event->name,
-    'startDate' => $startDate,
-    'endDate' => $endDate,
-  ];
+  if ($event->requestor) {
+    $template = new Template(EmailTemplates::get('Email Requestor Event Deleted'));
+    $templateData = [
+      'name' => $event->requestor->firstName,
+      'eventName' => $event->name,
+      'startDate' => $startDate,
+      'endDate' => $endDate,
+    ];
 
-  $email = new Email();
-  if ($config->email->sendRequestorMessagesTo == 'requestor') {
-    if ($event->requestor) $email->addRecipient($event->requestor->emailAddress);
-  } else {
-    $email->addRecipient($config->email->sendRequestorMessagesTo);
+    $email = new Email();
+    if ($config->email->sendRequestorMessagesTo == 'requestor') {
+      if ($event->requestor) $email->addRecipient($event->requestor->emailAddress);
+    } else {
+      $email->addRecipient($config->email->sendRequestorMessagesTo);
+    }
+    if ($config->email->copyAllEmail) $email->addBCC($config->email->copyAllEmail);
+    $email->addReplyTo($me->emailAddress, $me->getName());
+    $email->setSubject('Confirmation of cancellation/deletion of event: '.$event->name);
+    $email->setContent($template->render($templateData));
+    $email->sendText();
   }
-  if ($config->email->copyAllEmail) $email->addBCC($config->email->copyAllEmail);
-  $email->addReplyTo($me->emailAddress, $me->getName());
-  $email->setSubject('Confirmation of cancellation/deletion of event: '.$event->name);
-  $email->setContent($template->render($templateData));
-  $email->sendText();
-
 
   // Email the driver(s)
+  if (!$event->drivers) return;
   $template = new Template(EmailTemplates::get('Email Driver Event Deleted'));
   foreach ($event->drivers as $driverId) {
     if (!$driverId) continue;
--- api/post.confirm-event.php
+++ api/post.confirm-event.php
@@ -26,47 +26,51 @@ if (!$event->confirmed) {
 
 
   // Email to the requestor
-  $template = new Template(EmailTemplates::get('Email Requestor New Event'));
-  $templateData = [
-    'name' => $event->requestor->firstName,
-    'eventName' => $event->name,
-    'startDate' => Date('m/d/Y', strtotime($event->startDate)),
-    'endDate' => Date('m/d/Y', strtotime($event->endDate)),
-  ];
-
-  $email = new Email();
-  if ($config->email->sendRequestorMessagesTo == 'requestor') {
-    if ($event->requestor) $email->addRecipient($event->requestor->emailAddress, $event->requestor->getName());
-  } else {
-    $email->addRecipient($config->email->sendRequestorMessagesTo);
-  }
-  if ($config->email->copyAllEmail) $email->addBCC($config->email->copyAllEmail);
-  $email->addReplyTo($me->emailAddress, $me->getName());
-  $email->setSubject('Information regarding event: '.$event->name);
-  $email->setContent($template->render($templateData));
-  $email->sendText();
-
-
-  // Email the drivers
-  $template = new Template(EmailTemplates::get('Email Driver New Event'));
-  foreach ($event->drivers as $driverId) {
-    $driver = new User($driverId);
+  if ($event->requestor) {    
+    $template = new Template(EmailTemplates::get('Email Requestor New Event'));
     $templateData = [
-      'name' => $driver->firstName,
+      'name' => $event->requestor->firstName,
       'eventName' => $event->name,
       'startDate' => Date('m/d/Y', strtotime($event->startDate)),
       'endDate' => Date('m/d/Y', strtotime($event->endDate)),
     ];
 
     $email = new Email();
-    $email->addRecipient($driver->emailAddress, $driver->getName());
+    if ($config->email->sendRequestorMessagesTo == 'requestor') {
+      if ($event->requestor) $email->addRecipient($event->requestor->emailAddress, $event->requestor->getName());
+    } else {
+      $email->addRecipient($config->email->sendRequestorMessagesTo);
+    }
     if ($config->email->copyAllEmail) $email->addBCC($config->email->copyAllEmail);
     $email->addReplyTo($me->emailAddress, $me->getName());
-    $email->setSubject('An event has been assigned to you: '.$event->name);
+    $email->setSubject('Information regarding event: '.$event->name);
     $email->setContent($template->render($templateData));
-    $ical = $ics->to_string();
-    $email->AddStringAttachment("$ical", "calendar-item.ics", "base64", "text/calendar; charset=utf-8; method=REQUEST");
     $email->sendText();
+  }
+
+
+  // Email the drivers
+  if ($event->drivers) {
+    $template = new Template(EmailTemplates::get('Email Driver New Event'));
+    foreach ($event->drivers as $driverId) {
+      $driver = new User($driverId);
+      $templateData = [
+        'name' => $driver->firstName,
+        'eventName' => $event->name,
+        'startDate' => Date('m/d/Y', strtotime($event->startDate)),
+        'endDate' => Date('m/d/Y', strtotime($event->endDate)),
+      ];
+
+      $email = new Email();
+      $email->addRecipient($driver->emailAddress, $driver->getName());
+      if ($config->email->copyAllEmail) $email->addBCC($config->email->copyAllEmail);
+      $email->addReplyTo($me->emailAddress, $me->getName());
+      $email->setSubject('An event has been assigned to you: '.$event->name);
+      $email->setContent($template->render($templateData));
+      $ical = $ics->to_string();
+      $email->AddStringAttachment("$ical", "calendar-item.ics", "base64", "text/calendar; charset=utf-8; method=REQUEST");
+      $email->sendText();
+    }
   }
 }
 
