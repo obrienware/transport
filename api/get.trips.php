e5c6e41e0f575f9d5ae115d2a338f39363cd700d
Richard OBrien <richard@obrienware.com>
2025-02-17 15:07:47 -0700

    Fixed: Trips with null start_date was causing the calendar to crash.

--- api/get.trips.php
+++ api/get.trips.php
@@ -44,6 +44,8 @@ $query = "
       (end_date BETWEEN :start AND :end)
     )
     AND t.archived IS NULL
+    AND start_date IS NOT NULL
+    AND end_date IS NOT NULL
     AND t.cancellation_requested IS NULL
     {$criteria}
 ";
